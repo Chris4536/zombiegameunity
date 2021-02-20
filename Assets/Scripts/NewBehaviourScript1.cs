cfd4608bfa889881221a8fb49b748998d956adb9
Chris4536 <59596505+Chris4536@users.noreply.github.com>
2021-02-20 19:45:05 +0200

    Add files via upload

---
+++ Assets/Scripts/NewBehaviourScript1.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+    //zombie kood
+    public GameObject gameObjectPlayer;
+
+    void Update() {
+        Vector3 targetPosition = new Vector3(gameObjectPlayer.transform.position.x, transform.position.y, gameObjectPlayer.transform.position.z);
+        transform.LookAt(targetPosition);
+
+        transform.position = Vector3.Lerp(transform.position, gameObjectPlayer.transform.position, Time.deltaTime * 0.5f);
+    }
+}
---
+++ Assets/Scripts/NewBehaviourScript2.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+//spawner kood
+public class NewBehaviourScript2 : MonoBehaviour {
+
+    public GameObject templateZombie;
+    public GameObject templateCoin;
+
+    float time = 0;
+    int threshold = 5;
+
+    GameObject lastCoinGameObject = null;
+
+    void Update() {
+        time += Time.deltaTime;
+        if (time > threshold) {
+            GameObject newZombieGameObject = Instantiate(templateZombie);
+            newZombieGameObject.transform.position = transform.position;
+
+            if (lastCoinGameObject == null) { 
+                lastCoinGameObject = Instantiate(templateCoin);
+
+                int randomX = Random.Range(-4, 4);
+                int randomZ = Random.Range(-4, 4);
+
+                lastCoinGameObject.transform.position = new Vector3(randomX, transform.position.y, randomZ);
+        }
+            time = 0;
+        }
+
+        
+    
+    }
+
+}
